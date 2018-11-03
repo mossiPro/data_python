6fa45916412bdb7690530408752b96a0ad51aec1
Mossi <mossi.hashemi@gmail.com>
2018-11-03 14:57:50 +0100

    add code for open and read a csv file

--- parite.py
+++ parite.py
@@ -1,9 +1,17 @@
 #! /user/bin/env Python3
 #coding: utf-8
+import os
+import csv 
 
-def main():
-	pass
+def launch_analysis(data_file):
+    directory = os.path.dirname(__file__) # we get the right path.
+    path_to_file = os.path.join(directory, "data", data_file) # with this path, we go inside the folder `data` and get the file.
 
-if __name__ == '__main__':
-	main()
+    with open(path_to_file,"r") as f:
+        preview = f.readline()
 
+    print("Yeah! We managed to read the file. Here is a preview: {}".format(preview))
+
+
+if __name__ == "__main__":
+    launch_analysis('current_mps.csv')
